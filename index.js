45ab4acbd765c3176c1b0edb0e4313e35047c700
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-21 08:22:59 -0500

    Authentication Controller

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const http = require('http');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 // middlewares - any incoming request tot he server will be passed into these by default
--- router.js
+++ router.js
@@ -1,8 +1,15 @@
+const Authentication = require('./controllers/authentication');
+
 // export a function fron this file
 // import it to the index.js file and we wil passs app into that imported function
-// this way we will have acceess to app in this file
+// this way we will have access to app in this file
+
+  // module.exports = function(app){
+  //   app.get('/', function(req, res, next){
+  //     res.send(['waterBottle', 'paper'])
+  //   })
+  // }
+
 module.exports = function(app){
-  app.get('/', function(req, res, next){
-    res.send(['waterBottle', 'paper'])
-  })
+  app.post('/signup', Authentication.signup);
 }
